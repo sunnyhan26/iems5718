32f19d0b6f3b2aeb5cefeb697a01e1ab3b5a810e
Shing Yee <csy113@ie.cuhk.edu.hk>
2014-03-31 14:19:44 +0000

    Renamed omment.py and user.py to comment_func.py and user_func.py respectively

--- comment_func.py
+++ comment_func.py
@@ -1,8 +1,12 @@
-import webapp2
 from google.appengine.ext import ndb    
 
 class Comment(ndb.Model):
 	user = ndb.UserProperty(auto_current_user=True)
-	comment = ndb.TextProperty()
+	content = ndb.TextProperty()
 	time = ndb.DateTimeProperty(auto_now_add=True)
 
+def addComment(user, content, time):
+	comment = Comment(user=user, content, time)
+	comment.put()
+
+def getCommentList():
